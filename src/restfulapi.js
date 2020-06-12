69979503a71eae73203bf7b1eb14f5da0e07a42b
yuzin9712 <58067265+yuzin9712@users.noreply.github.com>
2020-06-12 18:19:46 +0900

    -

--- src/restfulapi.js
+++ src/restfulapi.js
@@ -1,4 +1,4 @@
 // 끝에 슬래시 넣지마시오!!!
 // good: "http://localhost:3000", bad: "http://localhost:3000/" <-끝에 슬래시있음
-export const sangminserver = "http://52.78.178.154/:8001/api";
-export const yujinserver = "http://52.78.178.154/:8001/api";
+export const sangminserver = "http://172.31.15.127/:8001/api";
+export const yujinserver = "http://172.31.15.127/:8001/api";
