d892f2bd29d6a0a258299de55de5b843a510221a
yuzin9712 <58067265+yuzin9712@users.noreply.github.com>
2020-06-24 23:38:18 +0900

    -

--- execute-deploy.sh
+++ execute-deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd /home/ubuntu
-sudo cp -r /home/ubuntu/client/client-latest/build /home/ubuntu/server/server-latest #build파일을 복사해서 서버폴더에 넣음
+sudo cp -r /home/ubuntu/client/client-latest/build /home/ubuntu/docker-image/server/server-latest #build파일을 복사해서 서버폴더에 넣음
 cd /home/ubuntu/docker-image #deploy.sh를 실행한다 무중단 배포
 ./deploy.sh > /dev/null 2> /dev/null < /dev/null &
